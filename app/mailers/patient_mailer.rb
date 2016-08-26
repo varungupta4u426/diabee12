67d8507f0e9b934446a5372a7ee7b8aef184f2c6
Varun Gupta <varun.gupta@mobiloittegroup.com>
2016-08-26 13:02:49 +0530

    after change in mailer credentilas

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -2,7 +2,7 @@ class PatientMailer < ApplicationMailer
 
 	def send_reset_password_link(patient)
 		@patient = patient
-    	mail(:to => patient.email, :subject => "password reset instructins",:from => "soberplaces.org")
+    	mail(:to => patient.email, :subject => "password reset instructins",:from => "varun@soberplaces.org")
 	end
 		
 end
