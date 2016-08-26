1188e65dc9017f9baa13264446f2dde11cb7923d
Varun Gupta <varun.gupta@mobiloittegroup.com>
2016-08-26 13:09:20 +0530

    after changing mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
-    :address              => "mail.soberplaces.org",
+    :address              => "mail.lptpl.com",
     :port                 => 25,
-    :domain               => 'soberplaces.org',
-    :user_name            => 'soberplacesorg',
-    :password             => 'Katie2014',
+    :domain               => 'lptpl.com',
+    :user_name            => 'varun@lptpl.com',
+    :password             => 'varun@123',
     :authentication       => 'plain',
     :openssl_verify_mode => 'none', 
     :enable_starttls_auto => true  }
