e5b955fcc3bd8507735ef7a869a2d1391a4526ee
Varun Gupta <varun.gupta@mobiloittegroup.com>
2016-08-29 17:42:42 +0530

    after removing doctor name

--- db/migrate/20160829102057_add_profile_and_doctor_to_patients.rb
+++ db/migrate/20160829102057_add_profile_and_doctor_to_patients.rb
@@ -1,6 +1,6 @@
 class AddProfileAndDoctorToPatients < ActiveRecord::Migration[5.0]
   def change
-    add_column :patients, :doctor_name, :string
+    # add_column :patients, :doctor_name, :string
     add_column :patients, :profile_pic, :string
   end
 end
