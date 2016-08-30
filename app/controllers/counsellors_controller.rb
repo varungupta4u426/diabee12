a899468bb82de631f7aae023756c2c3fc38c0422
Varun Gupta <varun.gupta@mobiloittegroup.com>
2016-08-30 18:44:11 +0530

    afetr change in assets

--- app/controllers/counsellors_controller.rb
+++ app/controllers/counsellors_controller.rb
@@ -9,11 +9,16 @@ class CounsellorsController < ApplicationController
 	end
 
 	def create
+		@counsellor = Counsellor.new(counsellor_params)
+
+		if @counsellor.save
+		else
+		end	
 	end	
 
 
 	def counsellor_params
+		params.require(:counsellor).permit(:first_name, :last_name, :email, :profile, :hcah_landline, :mobile)
 	end
 	
-		
 end
