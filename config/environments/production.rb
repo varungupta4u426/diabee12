b46ef9aa03704fdc09405a3bb5d38b9d7d7ebcab
Varun Gupta <varun.gupta@mobiloittegroup.com>
2016-09-01 10:46:57 +0530

    after change credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {:host => 'diabee12.herokuapp.com', protocol: 'http://' }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
   # config.action_mailer.smtp_settings = {
   #   :address              => "mail.lptpl.com",
